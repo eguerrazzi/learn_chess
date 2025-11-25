34e738d06689b848c06d928f02f408b517c45a5c
Emanuele Guerrazzi <e.guerrazzi@santannapisa.it>
2025-11-25 13:10:56 +0100

    Update

--- download_sounds.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-curl -L -o static/sounds/Move.mp3 https://raw.githubusercontent.com/lichess-org/lila/master/public/sound/standard/Move.mp3
-curl -L -o static/sounds/Capture.mp3 https://raw.githubusercontent.com/lichess-org/lila/master/public/sound/standard/Capture.mp3
-echo Done.
